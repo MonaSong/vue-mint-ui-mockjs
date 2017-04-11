a969b48fb286b069743dbda14a9086122ed5b272
宋青青 <1161500093@qq.com>
2017-04-11 13:31:04 +0800

    还是采用之前的方式处理setInterval

--- my-phone-app/src/pages/center.vue
+++ my-phone-app/src/pages/center.vue
@@ -288,10 +288,10 @@
         // 更新时间
         self.is_play = true
         this.set = setInterval(() => {
-          // if (!self.$refs || !self.$refs.music) {
-          //   clearInterval(self.set)
-          //   return ''
-          // }
+          if (!self.$refs || !self.$refs.music) {
+            clearInterval(self.set)
+            return ''
+          }
           this.updateTime()
           // 更新展示歌词
           this.updateLyr()
@@ -420,7 +420,10 @@
     },
     beforeRouteLeave (to, from, next) {
       clearInterval(this.set)
-      next()
+      next(vm => {
+        // 通过 `vm` 访问组件实例
+        clearInterval(vm.set)
+      })
     }
 
 }
