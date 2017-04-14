c4fe878c34b5a4d6ca8bcb85401dbefaaf853e00
宋青青 <1161500093@qq.com>
2017-04-14 18:04:40 +0800

    欢迎页面

--- my-phone-app/src/components/Hello.vue
+++ my-phone-app/src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="hello" :style="{height:win_h+'px'}">   
-        <p class="animate-hello" :class="{animated:animated_class,rubberBand:animated_class}"><img src="../../static/images/flower.png"></p>
-        <p :class="{animated:hua_h,bounceInUp:hua_h,'animate-h':hua_h,fade:leave}"><img src="../../static/images/flower-h.png" alt=""></p>
+  <div class="hello">   
+    <p class="animate-hello" :class="{animated:animated_class,rubberBand:animated_class}"><img src="../../static/images/flower.png"></p>
+    <p :class="{animated:hua_h,bounceInUp:hua_h,'animate-h':hua_h,fade:leave}"><img src="../../static/images/flower-h.png" alt=""></p>
   </div>
 </template>
 
@@ -29,11 +29,11 @@ export default {
   },
   methods: {
     skip () {
-      let self = this
-      setTimeout(() => {
-        self.$router.push('/home')
-        self.animated_class = false
-      }, 5000)
+      // let self = this
+      // setTimeout(() => {
+      //   self.$router.push('/home')
+      //   self.animated_class = false
+      // }, 5000)
     }
   }
 }
@@ -44,21 +44,30 @@ export default {
   .hello{
     width: 100%;
     height: 100%;
-    position: relative;
-    height: 600px;
+    position: fixed;
     overflow: hidden;
-    top: -80px;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
     overflow: hidden;
     .animate-hello{
       position: relative;
+      top: 100px;
       z-index: 11;
       overflow: hidden;
+      max-width: 300px;
+      text-align: center;
+      margin: 0 auto;
+      img{
+        width: 100%;
+      }
     }
     .animate-h{
       position: relative;
+      top: -300px;
       z-index: 10;
       opacity: 1;
-      top: -500px;
       img{
         display: block;
         position: relative;
